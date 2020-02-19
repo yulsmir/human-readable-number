ae7780ece24811e766370f4e9d25b2bcb5bc7123
julia <julianna.smirnova@gmail.com>
2020-02-19 22:18:29 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'nine'];
+    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'eighty', 'ninety'];
+
+    function getFullNumber(number) {
+
+    }
+
+    function getDozens() {
+
+    }
+
+    function getNumber(number) {
+        return number === 0 ? "zero" : getFullNumber(number);
+    }
+};
+
