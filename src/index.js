77f37dc0cb1ea91b780dfaf440c554c5cf4e8800
Yuliia <julianna.smirnova@gmail.com>
2020-03-02 22:49:14 +0200

    refactor: solve task

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 module.exports = function toReadable(number) {
-    let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'nine'];
-    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'eighty', 'ninety'];
+    let numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-    function getFullNumber(number) {
-        
+    function getHundreds(num) {
+        if (num > 99) {
+            return numbers[Math.floor(num / 100)] + " hundred " + getTens(num % 100);
+        } else {
+            return getTens(num);
+        }
     }
 
-    function getDozens() {
-
+    function getTens(num) {
+        if (num < 10) return numbers[num];
+        else if (num >= 10 && num < 20) return tens[num - 10];
+        else {
+            return teens[Math.floor(num / 10)] + " " + numbers[num % 10];
+        }
     }
 
-    function getNumber(number) {
-        return number === 0 ? "zero" : getFullNumber(number);
+    function getNumber(num) {
+        if (num === 0) return "zero";
+        else return getHundreds(num);
     }
+    return getNumber(number).trim();
 };
-
